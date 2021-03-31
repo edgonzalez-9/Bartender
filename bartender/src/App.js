bff391835de05b18938a4e1f84f42216c8c2b20e
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-03-31 18:42:37 -0500

    added folders for containers/components

--- bartender/src/App.js
+++ bartender/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import recipes from './Data.js'
 
 import 'semantic-ui-css/semantic.min.css'
+import DrinksContainer from './Containers/DrinksContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Missing Ingredient</h1>
-        {recipes.map(recipe => 
+        <DrinksContainer recipes={recipes}/>
+        {/* {recipes.map(recipe => 
           <div>
             <img src={recipe.image} className="App-logo" alt="logo" />
             <p>
@@ -22,15 +24,7 @@ function App() {
               Instructions - {recipe.instructions}
             </p>
           </div>
-          )}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+          )} */}
       </header>
     </div>
   );
---
+++ bartender/src/Containers/DrinksContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DrinksContainer extends React.Component {
+    render(){
+        <div>
+            
+        </div>
+    }
+}
+
+export default DrinksContainer;
