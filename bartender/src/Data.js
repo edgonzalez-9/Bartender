9f837a3a5029bae24fddfb5e6bcf59c1957d261a
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-04-06 12:49:55 -0500

    added yarn select

--- bartender/src/Data.js
+++ bartender/src/Data.js
@@ -3,30 +3,35 @@
 let data = [
     {
         name: 'El Diablo',
+        liquor: 'Tequila, Creme de Cassis', 
         ingredients: 'Tequila, Creme de Cassis, lime juice, ginger beer',
         instructions: 'Add the tequila, crème de cassis and lime juice into a shaker with ice and shake until well-chilled. Strain into a Collins glass over fresh ice and top with the ginger beer. Garnish with a lime wedge.',
         image: 'https://www.liquor.com/thmb/U5CNosrKMA-LNPp4PpW9fkx0awA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2016__03__28104153__el-diablo-720x720-recipe-e9f133dcfbc5488b8025374ee609ec0a.jpg'
     },
     {
         name: 'Rum and Coke',
-        ingredients: 'Coca-Cola, White Rum, lime juice', 
+        liquor: 'Rum',
+        ingredients: 'Coca-Cola, Rum, lime juice', 
         instructions: 'Fill glass with ice. Add rum and coke and stir to combine. Garnish with lime.',
         image: 'https://www.acouplecooks.com/wp-content/uploads/2020/11/Rum-and-Coke-005.jpg'
     },
     {
         name: 'Gin and Tonic',
+        liquor: 'Gin',
         ingredients: 'Gin, tonic water, ', 
         instructions: 'Fill a highball glass with ice and then add gin. Top with tonic water and stir gently.',
         image: 'https://cdn.diffords.com/contrib/stock-images/2016/8/07/201642335eb2fd8d5d9dc53a1fccb8dd1015.jpg'
     },
     {
         name: 'Vegas Bomb',
+        liquor: 'Crown Royal, Peach Schnapps',
         ingredients: 'Crown Royal, Peach Schnapps, cranberry juice, Red Bull or other energy drink', 
         instructions: 'Combine Crown Royal, Peach Schnapps, and cranberry juice in shaker. Pour into glass. Pour into a shot glass. Pour Red Bull into separate glass. Drop shot glass into Red Bull glass.',
         image: 'https://i2.chefiso.com/srv/images/vegasbomb-splash-coverblock-850x850.jpg'
     },
     {
         name: 'Whiskey Sour',
+        liquor: 'Bourbon',
         ingredients: 'Bourbon, fresh lemon juice, simple syrup.', 
         instructions: 'Combine bourbon, lime juice, and simple syrup in shaker. Pour into glass with ice.',
         image: 'https://assets.bonappetit.com/photos/57acc14e53e63daf11a4d9b6/1:1/w_2560%2Cc_limit/whiskey-sour.jpg'
