0d95347ce8aaf9a23543dd5b37c23e43f9781944
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-05-04 19:11:22 -0500

    card front has been redesigned!

--- bartender/src/Components/CardFront.js
+++ bartender/src/Components/CardFront.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function CardFront(props) {
     let recipe = props.recipe
+    let style = {
+        width: '18rem'
+    }
     return(
-        <div class="card border-light mb-3; w-25">
-                        <img src={recipe.image} class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h3 class="card-title">{recipe.name}</h3>
-                        </div>
-                        <button type="button" class="btn btn-primary" onClick={() => props.handleClick()}>Show Recipe</button>
-                    </div>
+        <div class="card" style={style}>
+            <img class="card-img-top" src={recipe.image} alt="..."/>
+            <div class="card-body">
+                <h3 class="card-title">{recipe.name}</h3>
+            </div>
+            <button type="button" class="btn btn-primary" onClick={() => props.handleClick()}>Show Recipe</button>
+        </div>
     )
 }
 
