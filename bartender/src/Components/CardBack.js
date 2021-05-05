1c30ff8959a39ebf0cab5ce839a2f9da162ccd6e
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-05-04 20:00:36 -0500

    fixed a small error in the button

--- bartender/src/Components/CardBack.js
+++ bartender/src/Components/CardBack.js
@@ -15,7 +15,7 @@ function CardBack(props) {
                         <p class="card-text">{recipe.ingredients}</p>
                         <p class="card-text">{recipe.instructions}</p>
                     </div>
-                    <button type="button" class="btn btn-primary" onClick={() => props.handleClick()}>HideRecipe</button>
+                    <button type="button" class="btn btn-primary" onClick={() => props.handleClick()}>Hide Recipe</button>
                 </div>
             </div>
         </div>
